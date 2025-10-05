9eb333cbcca550a444ddd92a6a3efbc595207afc
Vigas <77221759+vigasselvan@users.noreply.github.com>
2025-10-05 06:55:25 +0530

    remove the bug from the code
    incrementing should be done using += not =+, sorrected that code worked.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice(){
     
     if(randomVal <= 33){
         choice = "rock";
-    }else if(randomVal <= 66){
+    }else if(randomVal <= 66 && randomVal > 33){
         choice = "paper";
     }else{
         choice = "scissors";
@@ -19,43 +19,50 @@ function getHumanChoice(){
     let choiceVal = Number(prompt("Enter number: "));
     let humanChoice;
 
-
-    if(choiceVal == 1){
-        humanChoice = "rock";
-    }else if(choiceVal == 2){
-        humanChoice = "paper";
-    }else if(choiceVal == 3){
-        humanChoice = "scissors";
-    }else{
-        console.log("Please enter the correct option!");
+    switch(choiceVal){
+        case 1: humanChoice = "rock";
+                break;
+        case 2: humanChoice = "paper";
+                break;
+        case 3: humanChoice = "scissors";
+                break;
+        default: console.log("Please enter the correct option!");
     }
 
     return humanChoice;
 }
 
-let humanScore, computerScore = 0;
+let humanScore = 0; 
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice == computerChoice){
         console.log("Draw! Both have choosen same");
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "rock" && computerChoice == "paper"){
         console.log("You lose! Paper beats Rock");
-        computerScore =+ 1;
+        computerScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "rock" && computerChoice == "scissors"){
         console.log("You Won! Rock beats scissors");
-        humanScore =+ 1;
+        humanScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "paper" && computerChoice == "rock"){
         console.log("You Won! Paper beats rock");
-        humanScore =+ 1;
+        humanScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "paper" && computerChoice == "scissors"){
         console.log("You lose! scissors beats paper");
-        computerScore =+ 1;
+        computerScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "scissors" && computerChoice == "rock"){
         console.log("You lose! rock beats scissors");
-        computerScore =+ 1;
+        computerScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }else if(humanChoice == "scissors" && computerChoice == "paper"){
         console.log("You Won! scissors beats paper");
-        humanScore =+ 1;
+        humanScore++;
+        console.log("Human Score: " + humanScore + "\nComputer score: " + computerScore);
     }
 }
 
@@ -71,7 +78,7 @@ function playGame(){
         console.log("Congradulations, You Won!");
     }else if(humanScore < computerScore){
         console.log("Sorry, you lost in overall points!");
-    }else if(humanScore == computerScore){
+    }else if(humanScore === computerScore){
         console.log("Oops! its a tie");
     }
 }
